4fe54c0657fd56f13d31c225e7403fb196dc73f4
Dennis Haegler <d.haegler@hotmail.de>
2015-06-13 18:49:13 +0200

    Add new xml template with all the project member

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,20 +1,4 @@
 class DefaultController < ApplicationController
   def member
-    members = {
-      dennis: {
-        name: "Dennis H",
-        degree: "Master"
-      },
-
-      paul: {
-        name: "Paul K",
-        degree: "Master"
-      },
-      lucas: {
-        name: "Lucas",
-        degree: "Master"
-      },
-    }
-    render xml: members
   end
 end
