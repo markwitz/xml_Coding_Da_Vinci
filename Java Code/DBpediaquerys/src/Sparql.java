594d8bc78fce26f979907182d54b49a91765c509
Dennis Haegler <d.haegler@hotmail.de>
2015-06-23 09:40:42 +0200

    refactor code.
    Refactor this code to make it more readable for
    research.

--- Java Code/DBpediaquerys/src/Sparql.java
+++ Java Code/DBpediaquerys/src/Sparql.java
@@ -14,68 +14,47 @@ import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
 
-
-
-
-
-
-
-
-
 public class Sparql {
-	
-	
-	public static void main(String[] args) throws IOException {
-		ResultSet results = null;
-		QueryExecution qe = null;
-		List<String> strList= new ArrayList<String>();
-		strList.add("Karl Hagemeister");
-		strList.add("Carl Saltzmann");
-		String str;
-		String resultString = "hmm";
-        String service = "http://de.dbpedia.org/sparql";
-        
-        for (int i = 0; i < strList.size(); i++) {
-        	str = strList.get(i);
-	        String queryString = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n"+
-								"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n"+
-								"PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> \n"+
-								"SELECT ?artist ?info ?birth ?death ?piclink \n" +
-								"WHERE { \n"+
-								"?artist foaf:name \""+str+"\"@de . "+
-								"?artist rdfs:comment ?info. "+
-								"?artist dbpedia-owl:birthDate ?birth. "+
-								"?artist dbpedia-owl:deathDate ?death. "+
-								"?artist dbpedia-owl:thumbnail ?piclink. "+
-			"}";
-	        
-	
-	        Query query = QueryFactory.create(queryString);
-	        qe = QueryExecutionFactory.sparqlService(service, query);
-	        results = qe.execSelect();
-	        BufferedWriter out = new BufferedWriter(new FileWriter(str+".xml"));
-	        try{
-	        	
-	        	resultString = ResultSetFormatter.asXMLString(results);	
-	        	System.out.println(resultString);
-	        	
-	        	out.write(resultString);
-	        	
-	         } catch (IOException e) {
-				e.printStackTrace();
-			}
-	        finally{
-	           qe.close();
-	           out.close();
-	        }
-        };
-        
 
-	}
+  public static void main(String[] args) throws IOException {
+    ResultSet results = null;
+    QueryExecution qe = null;
+    List<String> strList= new ArrayList<String>();
+    strList.add("Karl Hagemeister");
+    strList.add("Carl Saltzmann");
+    String str;
+    String resultString = "hmm";
+    String service = "http://de.dbpedia.org/sparql";
 
-	
-	
+    for (int i = 0; i < strList.size(); i++) {
+      str = strList.get(i);
+      String queryString = "PREFIX foaf: <http://xmlns.com/foaf/0.1/> \n"+
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n"+
+        "PREFIX dbpedia-owl: <http://dbpedia.org/ontology/> \n"+
+        "SELECT ?artist ?info ?birth ?death ?piclink \n" +
+        "WHERE { \n"+
+        "?artist foaf:name \""+str+"\"@de . "+
+        "?artist rdfs:comment ?info. "+
+        "?artist dbpedia-owl:birthDate ?birth. "+
+        "?artist dbpedia-owl:deathDate ?death. "+
+        "?artist dbpedia-owl:thumbnail ?piclink. "+
+        "}";
 
-	
-	
+
+      Query query = QueryFactory.create(queryString);
+      qe = QueryExecutionFactory.sparqlService(service, query);
+      results = qe.execSelect();
+      BufferedWriter out = new BufferedWriter(new FileWriter(str+".xml"));
+      try{
+        resultString = ResultSetFormatter.asXMLString(results);
+        System.out.println(resultString);
+        out.write(resultString);
+      } catch (IOException e) {
+        e.printStackTrace();
+      } finally {
+        qe.close();
+        out.close();
+      }
+    };
+  }
 }
